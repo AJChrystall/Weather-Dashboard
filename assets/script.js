988d6bc98c66f189f60f20cbb2e1e892e4354b3e
Alexander Chrystall <zander@MacBook-Air-4.local>
2023-04-13 16:18:37 +1000

    I updated the script file so I can get 5 days and the html accordingly however still not working

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,42 @@
-const apiKey = '2ed76626b4e5f9735df0eba669822045'; // Replace with your OpenWeatherMap API key
-const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={2ed76626b4e5f9735df0eba669822045}'; // URL for current weather API endpoint
- 
-// Fetch weather data for a specific location
-function fetchWeatherData(location) {
-  fetch(`${apiUrl}?q=${location}&appid=${apiKey}`)
-    .then(response => response.json())
-    .then(data => {
-      // Process the weather data
-      console.log(data);
-      // Update your website's UI with the weather data
-      // Example: Display current temperature
-      const temperature = data.main.temp;
-      document.getElementById('temperature').innerText = `Temperature: ${temperature}°C`;
-    })
-    .catch(error => {
-      // Handle any errors
-      console.error(error);
-    });
+function addDays(date, days) {
+    const result = new Date(date);
+    result.setDate(result.getDate() + days);
+    return result;
 }
+  
+function fetchWeatherForecastData(location) {
+    const apiKey = '2ed76626b4e5f9735df0eba669822045'; // Actual API key
+    const startDate = addDays(new Date(), 5); // Get date after adding 5 days
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=${apiKey}&start_date=${startDate.toISOString()}`)
+      .then(response => response.json())
+      .then(data => {
+        // Process the weather forecast data
+        console.log(data);
+        // Update your website's UI with the weather forecast data
+        // Example: Display forecasted temperature, humidity, and wind speed
+        const forecastData = data.list;
+        const forecastContainer = document.getElementById('forecastData');
+        forecastContainer.innerHTML = ''; // Clear previous forecast data
+        forecastData.forEach(forecast => {
+          const forecastDate = new Date(forecast.dt * 1000); // Convert timestamp to date object
+          const forecastTemperatureKelvin = forecast.main.temp;
+          const forecastTemperatureCelsius = forecastTemperatureKelvin - 273.15;
+          const forecastHumidity = forecast.main.humidity;
+          const forecastWindSpeed = forecast.wind.speed;
+          const forecastElement = document.createElement('div');
+          forecastElement.innerHTML = `
+            <p>Date: ${forecastDate.toLocaleDateString()}</p>
+            <p>Temperature: ${forecastTemperatureCelsius.toFixed(2)}°C</p>
+            <p>Humidity: ${forecastHumidity}%</p>
+            <p>Wind Speed: ${forecastWindSpeed} m/s</p>
+          `;
+          forecastContainer.appendChild(forecastElement);
+        });
+      })
+      .catch(error => {
+        // Handle any errors
+        console.error(error);
+      });
+  }
+  
+  
