4d28a172fe733f36c9bba9ece34686750849f1d8
Alexander Chrystall <zander@MacBook-Air-4.local>
2023-04-13 12:54:04 +1000

    added the script file for weather api

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 const apiKey = '2ed76626b4e5f9735df0eba669822045'; // Replace with your OpenWeatherMap API key
-const apiUrl = 'https://api.openweathermap.org/data/2.5/weather'; // URL for current weather API endpoint
-
+const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={2ed76626b4e5f9735df0eba669822045}'; // URL for current weather API endpoint
+ 
 // Fetch weather data for a specific location
 function fetchWeatherData(location) {
   fetch(`${apiUrl}?q=${location}&appid=${apiKey}`)
